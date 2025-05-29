210a762ba2eb4be497117ab5bc9307f79e862b2e
Evelyn Aquiles <evelynaquiles22@outlook.com>
2025-05-29 15:52:40 -0300

    Add files via upload

---
+++ landPage/script.js
@@ -0,0 +1,35 @@
+function irparasobre() {
+    const imagem = document.getElementById("isobre");
+    const posicaoy = imagem.getBoundingClientRect().top;
+    window.scrollTo({
+        top: posicaoy,
+        behavior: "smooth"
+    }); 
+}
+
+function irparaserv() {
+    const imagem = document.getElementById('iserv');
+    const posicaoy = imagem.getBoundingClientRect().top;
+    window.scrollTo({
+        top: posicaoy,
+        behavior: "smooth"
+    }); 
+}
+
+function irparacontato() {
+    const imagem = document.getElementById('icontato');
+    const posicaoy = imagem.getBoundingClientRect().top;
+    window.scrollTo({
+        top: posicaoy,
+        behavior: "smooth"
+    }); 
+}
+
+function irparainicio() {
+    const imagem = document.getElementById('iinicio');
+    const posicaoy = imagem.getBoundingClientRect().top;
+    window.scrollTo({
+        top: posicaoy,
+        behavior: "smooth"
+    }); 
+}
